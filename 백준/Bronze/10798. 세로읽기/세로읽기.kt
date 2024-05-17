5124410d0b7b6aa38751a57f99e0b1dbb6f3184d
Lee Sanghyun <59863112+dev-ant@users.noreply.github.com>
2024-05-17 23:55:25 +0900

    [Bronze I] Title: 세로읽기, Time: 140 ms, Memory: 19524 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.kt
@@ -0,0 +1,24 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    var result = ""
+    var word = Array(5) { CharArray(15) { ' ' } }
+    var maxSize = 0
+    
+    for (i in 0 until 5) {
+        val input = next()
+        maxSize = Math.max(maxSize, input.length)
+        word[i] = input.toCharArray()
+    }
+    
+    for (i in 0 until maxSize) {
+        for (j in 0 until 5) {
+            if (word[j].getOrNull(i) == null) {
+                continue
+            }
+            result += word[j][i]
+        }
+    }
+    
+    print(result)
+}
