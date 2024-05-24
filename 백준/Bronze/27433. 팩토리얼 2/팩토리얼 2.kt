c82976eff318b1cca53e3b0ef74f91b518da493b
Lee Sanghyun <59863112+dev-ant@users.noreply.github.com>
2024-05-24 21:04:47 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 88 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
@@ -3,14 +3,14 @@ import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val N = readLine().toLong()
-    println(factorial(N))
+    println(when {
+        N >= 1 -> factorial(N)
+        else -> 1
+    })
 }
 
 fun factorial(num: Long): Long {
-    return if(num >= 1) {
-        num * factorial(num - 1)
-    }
-    else {
-        1
-    }
+    var result = num
+    if (num > 1) result *= factorial(num - 1)
+    return result
 }
