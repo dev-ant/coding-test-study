b6a73c08c6e86b4a60352476248f83619752889e
Lee Sanghyun <59863112+dev-ant@users.noreply.github.com>
2024-05-26 17:54:54 +0900

    [Bronze II] Title: 수 정렬하기, Time: 196 ms, Memory: 18616 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
@@ -3,13 +3,12 @@ import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val N = readLine().toInt()
-    val list = mutableListOf<Int>()
-
-    repeat(N) {
-        list.add(readLine().toInt())
+    val arr = ArrayList<Int>()
+    for(i in 0 until N) {
+        arr.add(readLine().toInt())
     }
-
-    list.sorted().forEach {
-        println(it)
+    val arrSorted = arr.sorted()
+    for(i in 0 until N) {
+        println(arrSorted[i])
     }
 }
