6dc11778fefc3c3f76ae41da87c326b7b88330d3
Lee Sanghyun <59863112+dev-ant@users.noreply.github.com>
2024-06-15 11:01:31 +0900

    [goormlevel] Title: 합 계산기, Time: 0.12 ms, Memory: 30.96 MB, Difficulty: 1 -BaekjoonHub

--- goormlevel/195685/1. 합 계산기/합 계산기.kt
+++ goormlevel/195685/1. 합 계산기/합 계산기.kt
@@ -1,17 +1,21 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var result = 0
-    (1..readLine().toInt()).map {
-        val (numA, op, numB) = readLine().split(" ")
-        result += when(op) {
-            "+" -> numA.toInt() + numB.toInt()
-            "-" -> numA.toInt() - numB.toInt()
-            "/" -> numA.toInt() / numB.toInt()
-            "*" -> numA.toInt() * numB.toInt()
-            else -> 0
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val operationCount = readLine().toInt()
+    val result = (1..operationCount)
+        .map { readLine().split(" ") }
+        .sumOf { (a, op, b) ->
+            val x = a.toInt()
+            val y = b.toInt()
+            when (op) {
+                "+" -> x + y
+                "-" -> x - y
+                "/" -> x / y
+                "*" -> x * y
+                else -> 0
+            }
         }
-    }
-    print(result)
+
+    println(result)
 }
